655e9854348938098a11872cec63ee9a0e55bff2
omerakcan9 <omerakcan34@proton.me>
2024-12-18 21:57:52 +0300

    #1 issue fixed

--- src/components/Task1/Task.jsx
+++ src/components/Task1/Task.jsx
@@ -35,12 +35,17 @@ function Task() {
         <div className='input_text_wrapper d-flex flex-column gap-2'>
           <div>
             <label className='form-label' htmlFor="example_1">Label :</label>
-            <input id='example_1' className='form-control bg-transparent text-white' type="text" />
+            <input className='form-control bg-transparent text-white'
+              id='example_1'
+              type="text"
+            />
             <p className='input_message mt-2'>Please write label to here </p>
           </div>
           <div>
-            <label className='form-label' htmlFor="example_1">Key :</label>
-            <input id='example_1' className='form-control bg-transparent text-white' type="password" />
+            <label className='form-label' htmlFor="example_2">Key :</label>
+            <input className='form-control bg-transparent text-white'
+              id='example_2'
+              type="password" />
             <p className='input_message mt-2'>Please write key to here </p>
           </div>
         </div>
@@ -49,17 +54,17 @@ function Task() {
           <div className='d-flex gap-3'>
             <div className='d-flex gap-1'>
               <input className='form-check-input'
-                id='active'
+                id='status_active'
                 type="radio"
-                name='radio_group2' />
-              <label htmlFor="status">Active</label>
+                name='status' />
+              <label htmlFor="status_active">Active</label>
             </div>
             <div className='d-flex gap-1'>
               <input className='form-check-input'
-                id='passive'
+                id='status_passive'
                 type="radio"
-                name='radio_group2' />
-              <label htmlFor="status">Passive</label>
+                name='status' />
+              <label htmlFor="status_passive">Passive</label>
             </div>
           </div>
         </div>
